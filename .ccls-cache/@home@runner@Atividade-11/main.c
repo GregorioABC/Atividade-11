0c8789f90c7d6ece367f3a44042a32a5cd7766e6
GregorioABC <87394080+GregorioABC@users.noreply.github.com>
2023-10-22 23:58:50 +0000

    Feito

--- .ccls-cache/@home@runner@Atividade-11/main.c
+++ .ccls-cache/@home@runner@Atividade-11/main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+int buscaBinaria(int vetor[], int tamanho, int valor, int* iteracoes) {
+    int esquerda = 0;
+    int direita = tamanho - 1;
+    while (esquerda <= direita) {
+        int meio = esquerda + (direita - esquerda) / 2;
+        *iteracoes += 1;
+        if (vetor[meio] == valor) {
+            return meio;
+        }
+        if (vetor[meio] < valor) {
+            esquerda = meio + 1;
+        } else {
+            direita = meio - 1;
+        }
+    }
+    return -1;
+}
+int main() {
+    int vetor[] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+    int tamanho = sizeof(vetor) / sizeof(vetor[0]);
+    int iteracoes = 0;
+    int valor, resultado;
+    printf("Digite o valor a ser procurado: ");
+    scanf("%d", &valor);
+    resultado = buscaBinaria(vetor, tamanho, valor, &iteracoes);
+    if (resultado == -1) {
+        printf("Valor não encontrado no vetor.\n");
+    } else {
+        printf("Valor encontrado na posição %d.\n", resultado);
+    }
+    printf("Número de interações: %d\n", iteracoes);
+    return 0;
 }
